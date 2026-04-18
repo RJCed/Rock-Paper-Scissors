f04cff7b4cb140e38e38f1b5c9e732ab6864a583
rjced <256202735+RJCed@users.noreply.github.com>
2026-04-18 08:51:16 +0800

    add getHumanChoice function: return rock, paper, scissors

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+    
+    // (1) reprompt- VARIABLE equal to true
+    // (2) WHILE reprompt is equal to true
+        // (3) choice-VARIABLE is equal to prompt of user "rock", "paper", "scissors"
+        // (4) IF choice(Lower Case) is equal to "rock"
+            // RETURN "rock"
+            // reprompt is equal to false
+        // (5) ELSE IF choice(Lower Case) is equal to "paper" 
+            // RETURN "paper"
+            // reprompt is equal to false
+        // (6) ELSE IF choice(Lower Case) is equal to "scissors" 
+            // RETURN "scissors"
+            // reprompt is equal to false
+        // (7) ELSE 
+            // ALERT INVALID CHOICE
+            // reprompt equal to true
+
+    let reprompt = true; // (1)
+
+    while (reprompt == true) { // (2)
+        const choice = prompt("Make your choice! \nRock, Paper, or Scissors "); // (3)
+        if (choice.toLowerCase() == "rock"){ // (4)
+            return "rock";
+            reprompt = false
+        } else if (choice.toLowerCase() == "paper"){ //(5)
+            return "paper";
+            reprompt = false
+        } else if (choice.toLowerCase() == "scissors"){ // (6)
+            return "scissors";
+            reprompt = false
+        } else { //(7)
+            alert("Invalid Choice!")
+            reprompt = true
+        }
+    }
+}
+
+console.log("Human: " + getHumanChoice())
