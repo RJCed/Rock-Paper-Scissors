aae26103d79b94f9cf52c9cf106e898655bf631a
rjced <256202735+RJCed@users.noreply.github.com>
2026-05-11 06:45:30 +0800

    general layout of page

--- script.js
+++ script.js
@@ -1,156 +1,156 @@
-//Get Computer Choice
-function getComputerChoice(){
-    // randomGen VARIABLE generate number between 0 - 0.999...
-    const randomGen = Math.random()
+// //Get Computer Choice
+// function getComputerChoice(){
+//     // randomGen VARIABLE generate number between 0 - 0.999...
+//     const randomGen = Math.random()
     
-    // Assign each random number from 0 - 0.999... to rock, paper, or scissors
-    // This separates them into region as follows:
-    // Region 1/3 (0.333...) below RETURN "scissors"
-    // Region 2/3 (0.666...) below (until 1/3) RETURN "paper"
-    // Region 1 and below (until 2/3) RETURN "rock"
-    // ELSE RETURN "getComputerChoice error"
+//     // Assign each random number from 0 - 0.999... to rock, paper, or scissors
+//     // This separates them into region as follows:
+//     // Region 1/3 (0.333...) below RETURN "scissors"
+//     // Region 2/3 (0.666...) below (until 1/3) RETURN "paper"
+//     // Region 1 and below (until 2/3) RETURN "rock"
+//     // ELSE RETURN "getComputerChoice error"
 
-    if (randomGen < 1 / 3){ 
-        return "scissors";
-    } else if (randomGen < 2 / 3){
-        return "paper";
-    } else if (randomGen < 1){
-        return "rock";
-    } else {
-        return "getComputerChoice error";
-    }   
-}
+//     if (randomGen < 1 / 3){ 
+//         return "scissors";
+//     } else if (randomGen < 2 / 3){
+//         return "paper";
+//     } else if (randomGen < 1){
+//         return "rock";
+//     } else {
+//         return "getComputerChoice error";
+//     }   
+// }
 
-//Get Human Choice
-function getHumanChoice(){
+// //Get Human Choice
+// function getHumanChoice(){
 
-    let reprompt = true;
+//     let reprompt = true;
 
-    while (reprompt == true) {
+//     while (reprompt == true) {
         
-        // User Input
-        let choice = prompt("Make your choice! \nRock, Paper, or Scissors "); // (3)
+//         // User Input
+//         let choice = prompt("Make your choice! \nRock, Paper, or Scissors "); // (3)
 
 
-        //CHECK IF EQUALS TO NULL (USER CLICKED CLOSE) and if not, make input lower case
-        if (choice == null){
-            alert("Finish the Game")
-            continue;
-        } else {
-            choice = choice.toLowerCase()
-        }
+//         //CHECK IF EQUALS TO NULL (USER CLICKED CLOSE) and if not, make input lower case
+//         if (choice == null){
+//             alert("Finish the Game")
+//             continue;
+//         } else {
+//             choice = choice.toLowerCase()
+//         }
 
-        //Returns the User's Input (Rock, Paper, Scissors)
-        if (choice == "rock"){ // convert this into switch in the future, but for now it works
-            return "rock";
-            reprompt = false
-        } else if (choice == "paper"){
-            return "paper";
-            reprompt = false
-        } else if (choice == "scissors"){
-            return "scissors";
-            reprompt = false
-        } else { //Take into account user not choosing Rock, Paper, or Scissors (also undefined)
-            alert("Invalid Choice!")
-            reprompt = true
-        }
-    }
-}
+//         //Returns the User's Input (Rock, Paper, Scissors)
+//         if (choice == "rock"){ // convert this into switch in the future, but for now it works
+//             return "rock";
+//             reprompt = false
+//         } else if (choice == "paper"){
+//             return "paper";
+//             reprompt = false
+//         } else if (choice == "scissors"){
+//             return "scissors";
+//             reprompt = false
+//         } else { //Take into account user not choosing Rock, Paper, or Scissors (also undefined)
+//             alert("Invalid Choice!")
+//             reprompt = true
+//         }
+//     }
+// }
 
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
 
-//Logic for each Round
-function playRound(humanChoice, computerChoice){
-    // IF humanChoice is "rock" and computer choice is "scissors"
-        // add one to human score
-        // Alert You Win (Rock Beats Scissors)
+// //Logic for each Round
+// function playRound(humanChoice, computerChoice){
+//     // IF humanChoice is "rock" and computer choice is "scissors"
+//         // add one to human score
+//         // Alert You Win (Rock Beats Scissors)
 
-    // ELSE IF humanChoice is "rock" and computer choice is "paper"
-        // add one to computer score
-        // Alert You Lose (Paper Beats Rock)
+//     // ELSE IF humanChoice is "rock" and computer choice is "paper"
+//         // add one to computer score
+//         // Alert You Lose (Paper Beats Rock)
 
-    // ELSE IF (DO THIS TO PAPER AND SCISSORS AS WELL)
-    // ELSE (humanChoice and computerChoice are equal)
-        // Alert TIE
+//     // ELSE IF (DO THIS TO PAPER AND SCISSORS AS WELL)
+//     // ELSE (humanChoice and computerChoice are equal)
+//         // Alert TIE
     
 
     
-    if (humanChoice == "rock" && computerChoice == "scissors"){ // FOR HUMAN ROCK
-        humanScore = humanScore + 1;
-        alert("You Win! Rock Beats Scissors")
-    } else if (humanChoice == "rock" && computerChoice == "paper"){
-        computerScore = computerScore + 1;
-        alert("You Lose! Paper Beats Rock")
+//     if (humanChoice == "rock" && computerChoice == "scissors"){ // FOR HUMAN ROCK
+//         humanScore = humanScore + 1;
+//         alert("You Win! Rock Beats Scissors")
+//     } else if (humanChoice == "rock" && computerChoice == "paper"){
+//         computerScore = computerScore + 1;
+//         alert("You Lose! Paper Beats Rock")
 
 
-    } else if (humanChoice == "paper" && computerChoice == "rock"){ // FOR HUMAN PAPER
-        humanScore = humanScore + 1;
-        alert("You Win! Paper Beats Rock")
-    } else if (humanChoice == "paper" && computerChoice == "scissors"){
-        computerScore = computerScore + 1;
-        alert("You Lose! Scissors Beats Paper")
+//     } else if (humanChoice == "paper" && computerChoice == "rock"){ // FOR HUMAN PAPER
+//         humanScore = humanScore + 1;
+//         alert("You Win! Paper Beats Rock")
+//     } else if (humanChoice == "paper" && computerChoice == "scissors"){
+//         computerScore = computerScore + 1;
+//         alert("You Lose! Scissors Beats Paper")
 
 
-    } else if (humanChoice == "scissors" && computerChoice == "paper"){ // FOR HUMAN SCISSORS
-        humanScore = humanScore + 1;
-        alert("You Win! Scissors Beats Paper")
-    } else if (humanChoice == "scissors" && computerChoice == "Rock"){
-        computerScore = computerScore + 1;
-        alert("You Lose! Rock Beats Scissors")
-    } else {
-        alert("Its a Tie!")
-    }
-
-    
-    
+//     } else if (humanChoice == "scissors" && computerChoice == "paper"){ // FOR HUMAN SCISSORS
+//         humanScore = humanScore + 1;
+//         alert("You Win! Scissors Beats Paper")
+//     } else if (humanChoice == "scissors" && computerChoice == "Rock"){
+//         computerScore = computerScore + 1;
+//         alert("You Lose! Rock Beats Scissors")
+//     } else {
+//         alert("Its a Tie!")
+//     }
 
     
     
-}
 
-
-//Plays round for a specific amount
-function playGame(rounds) {
-    // FOR each iteration (depends on how many rounds)
-        //run playRound
-    //Get the Total Score 
-
-    for(let i = 0; i < rounds; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
     
-    //Win or lose condition (Alert Who has the higher score Human vs Computer)
-    if (humanScore > computerScore){
-        alert("YOU WIN!\n" + "You: "+ humanScore + " Computer: " + computerScore)
-    } else if (humanScore < computerScore){
-        alert("YOU LOSE!\n" + "You: "+ humanScore + " Computer: " + computerScore)
-    } else {
-        alert("It's a tie!\n" + "You: "+ humanScore + " Computer: " + computerScore)
-    }
     
-}
+// }
 
 
-// This ask the user how many rounds are to be played
-let isInt = false;
-let rounds = 0;
-while (isInt == false){
-    let numRound = prompt("How many rounds would you like to play? ")
+// //Plays round for a specific amount
+// function playGame(rounds) {
+//     // FOR each iteration (depends on how many rounds)
+//         //run playRound
+//     //Get the Total Score 
 
-    if (isNaN(numRound)){
-        alert("Not A Number")
-        continue;
-    } else if (numRound == 0 || numRound == null){
-        alert("Invalid Number")
-        continue;
-    } else {
-        rounds = numRound;
-        isInt = true;
-    }
+//     for(let i = 0; i < rounds; i++){
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
     
-}
+//     //Win or lose condition (Alert Who has the higher score Human vs Computer)
+//     if (humanScore > computerScore){
+//         alert("YOU WIN!\n" + "You: "+ humanScore + " Computer: " + computerScore)
+//     } else if (humanScore < computerScore){
+//         alert("YOU LOSE!\n" + "You: "+ humanScore + " Computer: " + computerScore)
+//     } else {
+//         alert("It's a tie!\n" + "You: "+ humanScore + " Computer: " + computerScore)
+//     }
+    
+// }
 
-playGame(rounds)
+
+// // This ask the user how many rounds are to be played
+// let isInt = false;
+// let rounds = 0;
+// while (isInt == false){
+//     let numRound = prompt("How many rounds would you like to play? ")
+
+//     if (isNaN(numRound)){
+//         alert("Not A Number")
+//         continue;
+//     } else if (numRound == 0 || numRound == null){
+//         alert("Invalid Number")
+//         continue;
+//     } else {
+//         rounds = numRound;
+//         isInt = true;
+//     }
+    
+// }
+
+// playGame(rounds)
